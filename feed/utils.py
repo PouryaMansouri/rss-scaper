b980bce4d76d24a70685d2aeb4c3bbdf4355f594
Pourya Mansouri <pourya.mansouri@hotmail.com>
2023-03-26 12:27:07 +0430

    fetch feeds items from feed url func

--- feed/utils.py
+++ feed/utils.py
@@ -1,5 +1,16 @@
+from typing import List
+
+import aiohttp
+
 from feed.models import Feed
 
 
-def get_feeds_from_db():
+def get_feeds_from_db() -> List[Feed]:
     return Feed.objects.all()
+
+
+async def get_fetch_feeds_items_from_feed_url(feed: Feed):
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        async with session.get(feed.url) as resp:
+            resp = await resp.json()
+            return resp
