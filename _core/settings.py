81a98d3971a39056ce9bd473674dd2e5288d566b
Pourya Mansouri <pourya.mansouri@hotmail.com>
2023-03-26 19:37:42 +0430

    django settings

--- _core/settings.py
+++ _core/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/0",
+        "LOCATION": "redis://redis:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
