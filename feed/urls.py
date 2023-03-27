9ca1bf304ce2761539c9ffd8306a730f739eee9b
Pourya Mansouri <pourya.mansouri@hotmail.com>
2023-03-27 11:25:01 +0430

    force update

--- feed/urls.py
+++ feed/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (FeedList, FeedDetail)
+from .views import (FeedList, FeedDetail,UpdateFeeds)
 
 
 urlpatterns = [
     path('feeds/<int:pk>/', FeedDetail.as_view(), name='feed-detail'),
     path('feeds/', FeedList.as_view(), name='feed-list'),
+    path('force-update/', UpdateFeeds.as_view(), name='force-update')
 ]
 
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 from .models import Feed
 from .serializers import FeedSerializer
 
@@ -14,3 +16,14 @@ class FeedDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Feed.objects.all()
     serializer_class = FeedSerializer
     permission_classes = [AllowAny]
+
+
+class UpdateFeeds(generics.GenericAPIView):
+
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        from .tasks import update_feeds
+        update_feeds.delay()
+        message = "Feeds are being updated"
+        return Response(message, status=status.HTTP_200_OK)
